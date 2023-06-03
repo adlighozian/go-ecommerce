cb8e1f3edfc3182988e386082da260310661ae4e
adlighozian <adlighozian1@gmail.com>
2023-06-03 14:05:33 +0700

    add product image

--- consumer-insert-product/model/model.go
+++ consumer-insert-product/model/model.go
@@ -5,21 +5,27 @@ import (
 )
 
 type Product struct {
-	Id          int       `json:"id"`
-	StoreID     int       `json:"store_id"`
-	CategoryID  int       `json:"category_id"`
-	SizeID      int       `json:"size_id"`
-	ColorID     int       `json:"color_id"`
-	Name        string    `json:"name"`
-	Brand       string    `json:"brand"`
-	Subtitle    string    `json:"subtitle"`
-	Description string    `json:"description"`
-	UnitPrice   float64   `json:"unit_price"`
-	Status      bool      `json:"status"`
-	Stock       int       `json:"stock"`
-	Sku         string    `json:"sku"`
-	Weight      float64   `json:"weight"`
-	Created_at  time.Time `json:"created_at"`
-	Update_at   time.Time `json:"updated_at"`
+	Id           int            `json:"id"`
+	StoreID      int            `json:"store_id"`
+	CategoryID   int            `json:"category_id"`
+	SizeID       int            `json:"size_id"`
+	ColorID      int            `json:"color_id"`
+	Name         string         `json:"name"`
+	Brand        string         `json:"brand"`
+	Subtitle     string         `json:"subtitle"`
+	Description  string         `json:"description"`
+	UnitPrice    float64        `json:"unit_price"`
+	Status       bool           `json:"status"`
+	Stock        int            `json:"stock"`
+	Sku          string         `json:"sku"`
+	Weight       float64        `json:"weight"`
+	ProductImage []ProductImage `json:"image"`
+	Created_at   time.Time      `json:"created_at"`
+	Update_at    time.Time      `json:"updated_at"`
+}
+
+type ProductImage struct {
+	Name     string `json:"name"`
+	ImageURL string `json:"image_url"`
 }
 
--- product/repository/interface.go
+++ product/repository/interface.go
@@ -5,7 +5,7 @@ import "product-go/model"
 type Repositorier interface {
 	GetProduct(req model.ProductSearch) ([]model.Product, error)
 	ShowProduct(id int) (model.Product, error)
-	CreateProduct(req []model.ProductReq) ([]model.Product, error)
+	CreateProduct(req []model.Product) ([]model.Product, error)
 	UpdateProduct(req model.ProductUpd) (model.Product, error)
 	DeleteProduct(id int) (int, error)
 }
